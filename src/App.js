bd4d6742d3a92c5fc33fd724d246469da8b7efa7
Shivendra Kumar Sonkar <60128893+Shivendra-99@users.noreply.github.com>
2024-04-13 18:05:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
 import { getUsers } from './reduxSlice';
 import { useDispatch, useSelector } from 'react-redux';
 function App() {
-  const [userData , setUserData] =  useState([]);
+  const state = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
- useEffect(() => {
-   dispatch(getUsers());
-   setUserData(state.user.users);
- }, [dispatch,state.user.users])
   return (
     <div className="App">
-      {state.user.isLoading && <><b>Loading...</b></>}
-      {state.user.error && <h1>Error...</h1>}
-      {userData.map((user) => {
+      <button onClick={()=>dispatch(getUsers())}>click to Load the data</button>
+      {state.isLoading && <><b>Loading...</b></>}
+      {state.error && <h1>Error...</h1>}
+      {state.users.map((user) => {
         return <h1>{user.name} </h1>
       })}
     </div>
